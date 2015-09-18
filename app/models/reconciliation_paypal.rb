87a12003224d0a439f9ad813487adb43bb21fd44
yixiao.li <liyixiaoqd@qq.com>
2015-09-18 10:36:26 +0800

    bug

--- app/models/reconciliation_paypal.rb
+++ app/models/reconciliation_paypal.rb
@@ -165,7 +165,7 @@ class ReconciliationPaypal
 			}
 			response=''
 			Timeout::timeout(12){
-				response=rp.get_reconciliation((Time.now-60).gmtime.strftime("%Y-%m-%dT%H:%M:%SZ"),
+				response=get_reconciliation((Time.now-60).gmtime.strftime("%Y-%m-%dT%H:%M:%SZ"),
 					Time.now.gmtime.strftime("%Y-%m-%dT%H:%M:%SZ"),
 					params)
 			}
