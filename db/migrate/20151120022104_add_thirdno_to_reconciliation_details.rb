c4b1777a17059a51b2f1d067b8a22f47ba66fabe
yixiao.li <liyixiaoqd@qq.com>
2015-11-20 13:48:26 +0800

    add thirdno

--- db/migrate/20151120022104_add_thirdno_to_reconciliation_details.rb
+++ db/migrate/20151120022104_add_thirdno_to_reconciliation_details.rb
@@ -1,6 +1,6 @@
 class AddThirdnoToReconciliationDetails < ActiveRecord::Migration
   def change
-    add_column :reconciliation_details, :thirdno, :string
+    add_column :online_pays, :thirdno, :string
     add_column :online_pays, :credit_email, :string,:limit=>128
   end
 end
