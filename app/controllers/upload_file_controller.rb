ef2555daaced019376090a6611228f168cb50788
yixiao.li <liyixiaoqd@qq.com>
2015-12-29 11:49:18 +0800

    add xls xlsx support

--- app/controllers/upload_file_controller.rb
+++ app/controllers/upload_file_controller.rb
@@ -10,26 +10,29 @@ class UploadFileController < ApplicationController
 
 	def upload
 		begin
-			if params['file'].blank?
-				flash[:notice]="选择上传文件"
+			msg=check_param(params['file'],params['file_type'])
+
+			if msg.present?
+				flash[:notice]=msg
 			elsif params['file_type']=="sofort_transaction"
+				params['file'].original_filename
 				arr=CSV.parse(params['file'].read,{:col_sep => SOFORT_TRANSACTION_FILE_SPLIT})
 				reconciliation=ReconciliationSofort.new()
 				flash[:notice],flash[:error]=reconciliation.valid_reconciliation(arr)
 			elsif params['file_type']=="NL_ABN_Bank"
-				filename=write_file(params['file'],"xls")
+				filename=write_file(params['file'],"")
 				reconciliation=ReconciliationSofort.new()
 				flash[:notice],flash[:error]=reconciliation.valid_reconciliation_by_country("nl",filename)
 
 				File.delete(filename)
 			elsif params['file_type']=="DE_BOC_Bank"
-				filename=write_file(params['file'],"xlsx")
+				filename=write_file(params['file'],"")
 				reconciliation=ReconciliationSofort.new()
 				flash[:notice],flash[:error]=reconciliation.valid_reconciliation_by_country("de",filename)
 
 				File.delete(filename)
 			elsif params['file_type']=="AT_STA_Bank"
-				filename=write_file(params['file'],"xlsx")
+				filename=write_file(params['file'],"")
 				reconciliation=ReconciliationSofort
 				a,b=reconciliation.valid_reconciliation_by_country("at",filename)
 				flash[:notice]="对账处理:"+a if a.present?
@@ -72,9 +75,44 @@ class UploadFileController < ApplicationController
 	private 
 		def write_file(param_file,extension)
 			time_version=OnlinePay.current_time_format("%Y%m%d%H%M%S")
-			filename=Rails.root.join("upload_file","#{param_file.original_filename}_#{time_version}.#{extension}")
+			if extension.blank?
+				file_ext=File.extname(param_file.original_filename)
+			else
+				file_ext=extension
+			end
+
+			filename=Rails.root.join("upload_file","#{param_file.original_filename}_#{time_version}.#{file_ext}")
+
 			File.open(filename, "wb") { |f| f.write(param_file.read) }
 
 			filename
+		end
+
+		def check_param(param_file,file_type)
+			msg=''
+			if param_file.blank?
+				msg='选择上传文件'
+			elsif file_type.blank?
+				msg="选择上传业务类型"
+			else
+				file_ext=File.extname(param_file.original_filename)
+				if file_type=="sofort_transaction"
+					msg='请上传 .csv 格式文件' unless file_ext==".csv"
+				elsif file_type=="NL_ABN_Bank"
+					msg='请上传 .xlsx 或 .xls 格式文件' if file_ext!=".xlsx" && file_ext!=".xls"
+				elsif file_type=="DE_BOC_Bank"
+					msg='请上传 .xlsx 或 .xls 格式文件' if file_ext!=".xlsx" && file_ext!=".xls"
+				elsif file_type=="AT_STA_Bank"
+					msg='请上传 .xlsx 或 .xls 格式文件' if file_ext!=".xlsx" && file_ext!=".xls"
+				elsif file_type=="MERCHANT_EN_CASH_IN_BANK"
+					msg='请上传 .xlsx 格式文件' unless file_ext==".xlsx"
+				elsif file_type=="MERCHANT_NL_CASH_IN_BANK"
+					msg='请上传 .xlsx 格式文件' unless file_ext==".xlsx"
+				else
+					msg="未定义的上传业务类型,请重新选择"
+				end
+			end
+
+			msg
 		end
 end
