c5a9e43797a500213d7e498a1f20d31d0abb816a
yixiao.li <liyixiaoqd@qq.com>
2016-10-17 15:48:48 +0800

    交易手动支付,调用功能-增加支持paypal及相关bug修复

--- app/controllers/expection_handling_controller.rb
+++ app/controllers/expection_handling_controller.rb
@@ -24,25 +24,35 @@ class ExpectionHandlingController < ApplicationController
 					'sign'=>Digest::MD5.hexdigest("#{op.trade_no}#{Settings.authenticate.signkey}")		
 				}
 
+				#获取产生的财务流水ID
+				if op.system=="quaie"
+					fws=FinanceWater.where(user_id: op.user_id,operator: "system_submit",symbol: "Add",amount: op.amount).order("created_at desc")
+					if fws.present?
+						ret_hash['water_no']=fws[0].id
+					end
+				end
+
 				ret_hash['status']='success_notify'
 
 				redirect_notify_url=OnlinePay.redirect_url_replace("post",op.notification_url)
 				response=op.method_url_response("post",redirect_notify_url,false,ret_hash)
-				# if response.code=="200" && JSON.parse(response.body)['status']=="success"
+				if response.code=="200" && JSON.parse(response.body)['status']=="success"
 					op.set_status!("success_notify","")
 					op.reconciliation_detail.update_attributes!({:online_pay_status=>op.status}) unless op.reconciliation_detail.blank? 
+					op.save!()
+					flash[:notice]="手动调用成功,请进行手动对账"
+				else
+					flash[:notice]="手动调用失败,后台任务自动重试"
+				end
 				# else
 				# 	op.set_status!("failure_notify_third","recall info[#{response.code}:#{response.body}]")
-				# end
-				op.save!()
+				# end			
 			end
 		rescue => e
 			logger.info("#{op.order_no} recall_notify 调用错误:#{e.message}")
-			flash[:notice]="调用错误"
-			redirect_to index_online_pay_path(payway: op.payway,paytype: op.paytype,order_no: op.order_no) and return
+			flash[:notice]="调用错误:#{e.message}"
 		end
 
-		flash[:notice]="手动调用成功"
 		redirect_to index_online_pay_path(payway: op.payway,paytype: op.paytype,order_no: op.order_no)
 	end
 
@@ -65,13 +75,17 @@ class ExpectionHandlingController < ApplicationController
 		elsif op.payway=="sofort"
 			transactionid=op.trade_no
 		elsif op.payway=="paypal"
-			transactionid="EXPECTION_UNDEFINED"
+			transactionid=params[:transactionid]
 		else
 			transactionid="EXPECTION_UNDEFINED"
 		end
 	
-		if transactionid.blank?
-			flash[:notice]="获取财务对应记录错误,请确认"
+		if transactionid.blank? || transactionid=="null"
+			if op.payway=="paypal"
+				flash[:notice]="未输入支付交易号,请确认"
+			else
+				flash[:notice]="获取财务对应记录错误,请确认"
+			end
 			redirect_to index_online_pay_path(payway: op.payway,paytype: op.paytype,order_no: op.order_no) and return
 		end
 
@@ -84,7 +98,13 @@ class ExpectionHandlingController < ApplicationController
 		rd=ReconciliationDetail.find_by_transactionid(transactionid)
 		if rd.blank?
 			rd_new_flag=true
+		else
+			if rd.order_no.present? && rd.order_no!=op.order_no
+				flash[:notice]="支付交易号已存在,属于[#{rd.order_no}]"
+				redirect_to index_online_pay_path(payway: op.payway,paytype: op.paytype,order_no: op.order_no) and return	
+			end
 		end
+
 		logger.info("reconciliation_detail insert or update: #{rd_new_flag}")
 
 		begin
@@ -118,6 +138,7 @@ class ExpectionHandlingController < ApplicationController
 						rd.system=op.system
 						rd.reconciliation_flag="2"
 						rd.reconciliation_describe=desc
+						rd.online_pay_status=op.status
 
 						rd.update_attributes!({})
 					end
@@ -126,18 +147,23 @@ class ExpectionHandlingController < ApplicationController
 				
 				fw=FinanceWater.save_by_online_pay(op)
 				ret_hash['water_no']=fw.id unless fw.blank?
-				
-				redirect_url=OnlinePay.redirect_url_replace("post",op.notification_url)
-				if !op.method_url_success?("post",redirect_url,false,ret_hash)
-					raise "调用错误!"
+
+				redirect_notify_url=OnlinePay.redirect_url_replace("post",op.notification_url)
+				response=op.method_url_response("post",redirect_notify_url,false,ret_hash)
+				if response.code=="200" && JSON.parse(response.body)['status']=="success"
+					op.set_status!("success_notify","")
+					op.reconciliation_detail.update_attributes!({:online_pay_status=>op.status}) unless op.reconciliation_detail.blank? 
+					flash[:notice]="手动支付成功,请进行手动对账"
+				else
+					op.set_status!("failure_notify_third","recall info[#{response.code}:#{response.body}]")
+					flash[:notice]="财务系统支付成功,调用异常,转入后台任务自动重试"
 				end
+				op.save!()
 			end
 		rescue => e
 			flash[:notice]=e.message
-			redirect_to index_online_pay_path(payway: op.payway,paytype: op.paytype,order_no: op.order_no) and return
 		end
 
-		flash[:notice]="手动支付成功"
 		redirect_to index_online_pay_path(payway: op.payway,paytype: op.paytype,order_no: op.order_no)
 	end
 end
